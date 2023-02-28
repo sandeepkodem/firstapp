af9af290bcb6dcacb3103925ba1101d5128cbc53
sandeepkodem <55709335+sandeepkodem@users.noreply.github.com>
2023-02-28 10:21:02 +0530

    Update settings.py

--- clinicals/settings.py
+++ clinicals/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-lho+8%smtuxsclj9bdz&mc=tqx$-qw!n8h7i30f)qe1li-)1jw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True   
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
